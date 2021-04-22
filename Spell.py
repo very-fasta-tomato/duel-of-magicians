a6e5e0e942ca7ee222f479293d360bfe608d04dd
Timirbulatov <aidar126621@gmail.com>
2021-04-22 19:20:39 +0400

    created getters for spell class

--- Spell.py
+++ Spell.py
@@ -1,4 +1,22 @@
 class Spell:
-    def __init__(self, delta_enemy_hp, delta_enemy_mp):
+    def __init__(self, delta_enemy_hp, delta_enemy_mp, delta_ally_hp, delta_ally_mp, name):
         self.delta_enemy_hp = delta_enemy_hp
         self.delta_enemy_mp = delta_enemy_mp
+        self.delta_ally_hp = delta_ally_hp
+        self.delta_ally_mp = delta_ally_mp
+        self.name = name
+
+    def get_delta_enemy_hp(self):
+        return self.delta_enemy_hp
+
+    def get_delta_enemy_mp(self):
+        return self.delta_enemy_mp
+
+    def get_delta_ally_hp(self):
+        return self.delta_ally_hp
+
+    def get_delta_ally_mp(self):
+        return self.delta_ally_mp
+
+    def get_name(self):
+        return self.name
