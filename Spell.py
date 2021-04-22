765482ce04e3c0ed36de08d78608f9c773e6863e
Timirbulatov <aidar126621@gmail.com>
2021-04-22 19:50:58 +0400

    added comments

--- Spell.py
+++ Spell.py
@@ -1,10 +1,10 @@
 class Spell:
     def __init__(self, delta_enemy_hp, delta_enemy_mp, delta_ally_hp, delta_ally_mp, name):
-        self.delta_enemy_hp = delta_enemy_hp
-        self.delta_enemy_mp = delta_enemy_mp
-        self.delta_ally_hp = delta_ally_hp
-        self.delta_ally_mp = delta_ally_mp
-        self.name = name
+        self.delta_enemy_hp = delta_enemy_hp  # урон врагу
+        self.delta_enemy_mp = delta_enemy_mp  # урон мане врага
+        self.delta_ally_hp = delta_ally_hp  # урон себе
+        self.delta_ally_mp = delta_ally_mp  # потребление маны
+        self.name = name  # название
 
     def get_delta_enemy_hp(self):
         return self.delta_enemy_hp
