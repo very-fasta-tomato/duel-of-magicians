17e3ca470243693a5e661064b0ad4971822a05c7
Timirbulatov <aidar126621@gmail.com>
2021-05-10 13:00:55 +0400

    created rejim

--- SpellClass.py
+++ SpellClass.py
@@ -1,10 +1,10 @@
 class Spell:
-    def __init__(self, delta_enemy_hp, delta_enemy_mp, delta_ally_hp, delta_ally_mp, name):
+    def __init__(self, delta_enemy_hp, delta_enemy_mp, delta_ally_hp, delta_ally_mp, verojatnost_popadanija):
         self.delta_enemy_hp = delta_enemy_hp  # урон врагу
         self.delta_enemy_mp = delta_enemy_mp  # урон мане врага
         self.delta_ally_hp = delta_ally_hp  # урон себе
         self.delta_ally_mp = delta_ally_mp  # потребление маны
-        self.name = name  # название
+        self.verojatnost_popadanija = verojatnost_popadanija
 
     def get_delta_enemy_hp(self):
         return self.delta_enemy_hp
@@ -18,5 +18,5 @@ class Spell:
     def get_delta_ally_mp(self):
         return self.delta_ally_mp
 
-    def get_name(self):
-        return self.name
+    def get_verojatnost_popadanija(self):
+        return self.verojatnost_popadanija
---
+++ createObject.py
@@ -0,0 +1,21 @@
+import SpellClass
+import random
+
+
+def createclassic():
+    delta_enemy_hp = random.randint(1, 7)
+    delta_ally_mp = random.randint(1, 2)
+    spell1 = SpellClass.Spell(delta_enemy_hp, 0, 0, delta_ally_mp, 0, 0)
+    return spell1
+
+
+def createmaximum():
+    delta_enemy_hp = random.randint(-2, 7)
+    delta_ally_mp = random.randint(-2, 2)
+    delta_enemy_mp = random.randint(-2, 1)
+    delta_ally_hp = random.randint(-2, 1)
+    spell1 = SpellClass.Spell(delta_enemy_hp, delta_enemy_mp, delta_ally_hp, delta_ally_mp, 0, 0)
+    return spell1
+
+
+createclassic()
