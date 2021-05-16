20cc79296e5c945497738e68e6938bf2431ec9d0
lamarricane <froggsycopp@gmail.com>
2021-05-16 17:30:20 +0400

    интерфейс

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import CharacterClass
 import createObject
 import SpellClass
 from tkinter import*
+from tkinter import ttk
+from tkinter.ttk import*
 
 
 
 window = Tk()
 window.title("Битва магов")
 window.geometry('600x600')
-lbl = Label(window, text = "Битва Магов")
-lbl.grid(column = 1, row = 0)
-#btn1 = Button(window, text = "Начать игру")
-#btn1.grid(column = 2, row = 5)
-#btn2 = Button(window, text = "Выход", command = window.destroy())
-#btn2.grid(column = 5, row = 9)
-#btn3 = Button(window, text = "Статистика")
-#btn3.grid(column = 5, row = 6)
-#btn4 = Button(window, text = "Настройки")
-#btn4.grid(column = 5, row = 7)
+tab_control = ttk.Notebook(window)
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab3 = ttk.Frame(tab_control)
+tab_control.add(tab1, text='Игра', )
+tab_control.add(tab2, text='Статистика')
+tab_control.add(tab3, text='Настройки')
+chk_state = BooleanVar()
+chk_state.set(True)
+chk = Checkbutton(tab3, text='Включить звук', var=chk_state)
+chk.grid(column=0, row=0)
+btn11 = Button(tab1, text="Начать игру")
+btn11.grid(column=1, row=0)
+btn12 = Button(tab1, text="Выход", command=window.quit)
+btn12.grid(column=1, row=2)
+lbl2 = Label(tab2, text='Вкладка 2')
+lbl2.grid(column=0, row=0)
+tab_control.pack(expand=1, fill='both')
 window.mainloop()
