14897300e178bba6b0e290c032413c5b2aafef82
Timirbulatov <aidar126621@gmail.com>
2021-05-17 21:11:25 +0400

    начато объединение проекта

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import CharacterClass
 import createObject
-import SpellClass
-from tkinter import*
+from tkinter import *
 from tkinter import ttk
-from tkinter.ttk import*
-
-
+from tkinter.ttk import *
 
 root = Tk()
 
+
+def startgame(selected):
+    player = CharacterClass.Character()
+    if selected.get() == 1:
+        spell1 = createObject.createclassic()
+        spell2 = createObject.createclassic()
+        spell3 = createObject.createclassic()
+    if selected.get() == 2:
+        spell1=createObject.createmaximum()
+        spell2 = createObject.createmaximum()
+        spell3 = createObject.createmaximum()
+
+
+
 def command():
-    window2=Toplevel(root)
+    window2 = Toplevel(root)
     window2.title("Режимы игры")
     window2.geometry('450x450')
-    lbl = Label (window2, text="Выберите режим игры")
+    lbl = Label(window2, text="Выберите режим игры")
     lbl.grid(column=0, row=1)
-    rad1 = Radiobutton(window2, text='Традиционный', value=1)
-    rad2 = Radiobutton(window2, text='Нетрадиционный', value=2)
+    selected = IntVar()
+    rad1 = Radiobutton(window2, text='Традиционный', value=1, variable=selected)
+    rad2 = Radiobutton(window2, text='Нетрадиционный', value=2, variable=selected)
     rad1.grid(column=0, row=3)
     rad2.grid(column=1, row=3)
     btn21 = Button(window2, text="Начать игру")
@@ -42,6 +54,5 @@ btn11.grid(column=1, row=0)
 btn12 = Button(tab1, text="Выход", command=root.quit)
 btn12.grid(column=1, row=2)
 tab_control.pack(expand=1, fill='both')
-
 
 root.mainloop()
