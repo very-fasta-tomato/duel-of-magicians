8249a6c37591f4d6c60fb6a7e327ea5190c8dfc4
Timirbulatov <aidar126621@gmail.com>
2021-06-13 23:51:12 +0400

    клиент-сервер чата, вроде как должен работать с несколькими клиентами, я не проверял

--- client.py
+++ client.py
@@ -1,76 +1,23 @@
 import socket
+import threading
 import json
 import constructor
 import PySimpleGUI as sg  # сторонний GUI фреймворк
 
-winstat = 0
-losestat = 0
-conn = socket.socket()
 
-layout = [[sg.Button('Начать игру', size=(11, 1))],  # структура главного окна
-          [sg.Button('Статистика', size=(11, 1))],
-          [sg.Button('Настройки', size=(11, 1))],
-          [sg.Button('Выход', size=(11, 1))]]
+def read_sok():
+    while 1:
+        data = sor.recv(1024)
+        print(data.decode('utf-8'))
 
-layout2 = [[sg.Text('Сейчас ваш ход', key='TURN')],  # структура окна игры
-           [sg.Text('Ваши HP', key='-hp-')],
-           [sg.Text('Ваши MP', key='-mp-')],
-           [sg.Button('Заклинание 1', size=(21, 1)), sg.Button('Заклинание 2', size=(21, 1)),
-            sg.Button('Заклинание 3', size=(21, 1))],
-           [sg.Text('Урон', key='-deh1-', size=(21, 1)), sg.Text('Урон', key='-deh2-', size=(21, 1)),
-            sg.Text('Урон', key='-deh3-', size=(21, 1))],
-           [sg.Text('Стоимость', key='-dam1-', size=(21, 1)), sg.Text('Стоимость', key='-dam2-', size=(21, 1)),
-            sg.Text('Стоимость', key='-dam3-', size=(21, 1))],
-           [sg.Text('Урон мане', key='-dem1-', size=(21, 1)), sg.Text('Урон мане', key='-dem2-', size=(21, 1)),
-            sg.Text('Урон мане', key='-dem3-', size=(21, 1))],
-           [sg.Text('Восстановление HP', key='-dah1-', size=(21, 1)),
-            sg.Text('Восстановление HP', key='-dah2-', size=(21, 1)),
-            sg.Text('Восстановление HP', key='-dah3-', size=(21, 1))],
-           [sg.Text('Вероятность попадания', key='-vp1-', size=(21, 1)),
-            sg.Text('Вероятность попадания', key='-vp2-', size=(21, 1)),
-            sg.Text('Вероятность попадания', key='-vp3-', size=(21, 1))],
-           [sg.Button('Обновить данные')]]
 
-window = sg.Window('Дуэль магов', layout)  # объявление главного окна
-while True:
-    event, values = window.read()  # создание главного окна
-    if event == sg.WIN_CLOSED or event == 'Выход':
-        break
-    if event == 'Статистика':
-        event, values = sg.Window('Статистика',
-                                  [[sg.Text("Победы", size=(9, 1)), sg.Text(str(winstat))],
-                                   [sg.Text("Поражения", size=(9, 1)), sg.Text(str(losestat))],
-                                   [sg.Button('Назад')]]).read(close=True)
-        print(event, values)
-    if event == 'Настройки':
-        event, values = sg.Window('Настройки',
-                                  [[sg.Checkbox('Включить звук', enable_events=True, key='-sound-')],
-                                   [sg.Button('Назад')]]).read(close=True)
-        print(event, values)
-    if event == 'Начать игру':
-        event, values = sg.Window('Поиск оппонента',
-                                  [[sg.Text('Введите IP сервера')],
-                                   [sg.Input(key='-IP-')],
-                                   [sg.Button('OK')]]).read(close=True)
-        IP = values['-IP-']
-        conn.connect((IP, 14900))
-        event, values = sg.Window('Режимы игры',
-                                  [[sg.Text('Выберете режим игры')],
-                                   [sg.Button('Стандартный', size=(11, 1))],
-                                   [sg.Button('Расширенный', size=(11, 1))]]).read(close=True)
-        if event == 'Стандартный':
-            conn.send(json.dumps('Стандартный').encode())
-        if event == 'Расширенный':
-            conn.send(json.dumps('Расширенный').encode())
-        window2 = sg.Window('game', layout2)  # объявление окна с полем
-        wn2 = True
-        while wn2:
-            event, values = window2.read()  # создание окна с полем
-            if event == sg.WIN_CLOSED:
-                wn2 = False
-            if event == 'Заклинание 1':
-                conn.send(json.dumps('Заклинание 1').encode())
-            if event == 'Заклинание 2':
-                conn.send(json.dumps('Заклинание 2').encode())
-            if event == 'Заклинание 3':
-                conn.send(json.dumps('Заклинание 3').encode())
+server = '192.168.1.49', 9090  # Данные сервера
+alias = input()  # Вводим наш псевдоним
+sor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sor.bind(('', 0))  # Задаем сокет как клиент
+sor.sendto((alias + ' Connect to server').encode('utf-8'), server)  # Уведомляем сервер о подключении
+potok = threading.Thread(target=read_sok)
+potok.start()
+while 1:
+    mensahe = input()
+    sor.sendto(('[' + alias + ']' + mensahe).encode('utf-8'), server)
--- server.py
+++ server.py
@@ -4,65 +4,22 @@ import SpellClass
 import createObject
 import constructor
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # сокет для получение IP пользователя
+# AF_INET - используется IP-протокол четвертой версии. SOCK_STREAM - TCP
+s.connect(("gmail.com", 80))
+myIP = str(s.getsockname()[0])
+s.close()
 
-def launch_game(sock, clients):
-    player1 = CharacterClass.Character()
-    player2 = CharacterClass.Character()
-    #  начало игры
-    round_of_game = 0
-    while True:
-        round_of_game += 1
-        if round_of_game % 2 == 0:
-            _data, _addr = sock.recvfrom(1024)
-            if _addr == clients[0]:
-                player1_spell1 = createObject.createclassic()
-                player1_spell2 = createObject.createclassic()
-                player1_spell3 = createObject.createclassic()
-                if _data == 'Заклинание 1':
-                    player1.change_hp(player1_spell1.get_delta_ally_hp())
-                    player1.change_mp(player1_spell1.get_delta_ally_mp())
-                    player2.change_hp(player1_spell1.get_delta_ally_hp())
-                    player2.change_mp(player1_spell1.get_delta_ally_mp())
-                if _data == 'Заклинание 2':
-                    player1.change_hp(player1_spell2.get_delta_ally_hp())
-                    player1.change_mp(player1_spell2.get_delta_ally_mp())
-                    player2.change_hp(player1_spell2.get_delta_ally_hp())
-                    player2.change_mp(player1_spell2.get_delta_ally_mp())
-                if _data == 'Заклинание 3':
-                    player1.change_hp(player1_spell3.get_delta_ally_hp())
-                    player1.change_mp(player1_spell3.get_delta_ally_mp())
-                    player2.change_hp(player1_spell3.get_delta_ally_hp())
-                    player2.change_mp(player1_spell3.get_delta_ally_mp())
-        else:
-            _data, _addr = sock.recvfrom(1024)
-            if _addr == clients[1]:
-                player2_spell1 = createObject.createclassic()
-                player2_spell2 = createObject.createclassic()
-                player2_spell3 = createObject.createclassic()
-                if _data == 'Заклинание 1':
-                    player1.change_hp(player2_spell1.get_delta_ally_hp())
-                    player1.change_mp(player2_spell1.get_delta_ally_mp())
-                    player2.change_hp(player2_spell1.get_delta_ally_hp())
-                    player2.change_mp(player2_spell1.get_delta_ally_mp())
-                if _data == 'Заклинание 2':
-                    player1.change_hp(player2_spell2.get_delta_ally_hp())
-                    player1.change_mp(player2_spell2.get_delta_ally_mp())
-                    player2.change_hp(player2_spell2.get_delta_ally_hp())
-                    player2.change_mp(player2_spell2.get_delta_ally_mp())
-                if _data == 'Заклинание 3':
-                    player1.change_hp(player2_spell3.get_delta_ally_hp())
-                    player1.change_mp(player2_spell3.get_delta_ally_mp())
-                    player2.change_hp(player2_spell3.get_delta_ally_hp())
-                    player2.change_mp(player2_spell3.get_delta_ally_mp())
-
-
-clients = []
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("", 14900))
-sock.listen(2)  # максимальное число соединений в очереди
-while True:
-    data, addr = sock.recvfrom(1024)
-    if addr not in clients:
-        clients.append(addr)
-    if len(clients) == 2:
-        launch_game(sock, clients)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((myIP, 9090))
+client = []  # Массив где храним адреса клиентов
+print('Start Server')
+while 1:
+    data, addres = sock.recvfrom(1024)
+    print(addres[0], addres[1])
+    if addres not in client:
+        client.append(addres)  # Если такова клиента нету , то добавить
+    for clients in client:
+        if clients == addres:
+            continue  # Не отправлять данные клиенту который их прислал
+        sock.sendto(data, clients)
